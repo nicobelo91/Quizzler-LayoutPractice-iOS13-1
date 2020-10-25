7919d3a4d0823199a2a653df3680fb86b1476ad4
Nico Cobelo <ingcobelo@gmail.com>
2020-10-25 10:59:05 -0400

    Added third button and multiple choice questions

--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -11,9 +11,11 @@ import Foundation
 struct Question {
     let text: String
     let answer: String
+    let correctAnswer: String
 
-    init(q: String, a:String) {
+    init(q: String, a: String, correctAnswer: String) {
     text = q
     answer = a
+    self.correctAnswer = correctAnswer
     }
 }
